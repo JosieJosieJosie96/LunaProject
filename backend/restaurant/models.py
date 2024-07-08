3f2f7be782315c68ea1dc2d935b281fa9e6a5a58
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-08 18:16:13 +0200

    backend models - added models - tested makemigrations and migrations - tested superuser

--- backend/restaurant/models.py
+++ backend/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from project import settings
+
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    zip = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    opening_hours = models.CharField(max_length=100)
+    price_level = models.CharField()
+    image = models.ImageField(upload_to='restaurant_images')
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
