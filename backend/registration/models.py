bf2700a5b18440dba27e4283a255b9876d4832d3
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-08 18:18:06 +0200

    backend models - added models - tested makemigrations and migrations - tested superuser

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -1,3 +1,19 @@
+import string
+import random
+
 from django.db import models
 
-# Create your models here.
+from project import settings
+
+
+def code_generator(length=12):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+class Registration(models.Model):
+    registration_code = models.CharField(max_length=12, default=code_generator)
+    user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"registration with code {self.registration_code}"
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,19 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+from review.models import Review
 
 
 class User(AbstractUser):
-    pass
+    location = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
+    things_i_love = ArrayField(ArrayField(models.TextField()), null=True)
+    description = models.TextField(blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    profile_picture = models.ImageField(upload_to="profile_picture", blank=True)
+    banner_picture = models.ImageField(upload_to="banner_picture", blank=True)
+    likes = models.ManyToManyField(Review, blank=True, related_name="liked_by")
+
+
+
