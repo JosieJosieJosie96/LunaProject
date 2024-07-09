8501abec29510990f5dcb6694c53388dd80bf00c
Josie Fessey <07jreckn@gmail.com>
2024-07-09 16:09:35 +0300

    initial commit

---
+++ frontend/src/components/Profile.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+export const CommentsContainer = styled.div`
+bg-color: "black"
+`
+export const TopContainer = styled.div``
+
+export const ProfileLeftContainer = styled.div`
+width: 232px;
+height: 185px;
+top: -1px;
+  font-family: Roboto, sans-serif;
+  font-size: 18px;
+  font-weight: 300;
+  line-height: 21.09px;
+  text-align: left;
+  margin-top: 12px;
+  margin-left: 68px;
+border-bottom: 1px solid #979797
+    padding-bottom: 10px;
+`
+
+export const RightContainer = styled.div``
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,127 @@
+import { useParams } from 'react-router';
+// import useAutoFetch from '../hooks/useAutoFetch.js';
+import Header from '../components/Header.jsx';
+
+
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { setProfileFilter } from '../store/slices/profileFilter.js';
+import Footer from "../components/Footer.jsx";
+import {CommentsContainer, ProfileLeftContainer, RightContainer, TopContainer} from "../components/Profile.styled.js";
+
+
+
+
 function Profile() {
-  return <div>Profile</div>;
+  const { profileId } = useParams()
+  // const params = useParams()
+  // const endpointToFetch = profileId ? `users/${profileId}/` : 'users/me/'
+  // const { data, error } = useAutoFetch('get', endpointToFetch, '', params)
+  // const dispatch = useDispatch()
+
+// useEffect(() => {
+//   dispatch(setProfileFilter('posts'));
+// }, [dispatch]);
+
+
+  return (
+    <>
+      {/*{data && (*/}
+        <>
+          <Header /*userdata={data}*/ profileId={profileId} />
+          <div className={'banner-image'}>
+          <img src={'path/to/profile-image.jpg'} alt="Profile"/>
+        </div>
+
+        <ProfileLeftContainer>
+            <div className={'profileimage'}>
+                <img src={'path/to/profile-image.jpg'} alt="Profile"/>
+            </div>
+
+            <div className={'reviews'}>
+                <h3>Reviews</h3>
+
+            </div>
+            <div className={'comments'}>
+                <h3>Comments</h3>
+            </div>
+            <div className={'restaurants'}>
+                <h3>Restaurants</h3>
+
+            </div>
+            <div className={'edit profile'}>
+                <h3>Edit Profile</h3>
+
+            </div>
+
+
+        </ProfileLeftContainer>
+        <TopContainer>
+            <div className={'name'}>
+                {/*<p>{userdata.name}</p>*/}
+            </div>
+            <div className={'location'}>
+                {/*<p>{userdata.location}</p>*/}
+            </div>
+            <div className={'reviews'}>
+                {/*<p>{userdata.reviews}</p>*/}
+            </div>
+            <div className={'comments'}>
+                {/*<p>{userdata.comments}</p>*/}
+            </div>
+
+
+        </TopContainer>
+
+
+        <CommentsContainer>
+            <div className={'comments'}>
+                <h3>Comments</h3>
+            </div>
+            <div className={'review 1'}>
+                <h3>Review 1</h3>
+                {/*<p>{userdata.comments}</p>*/}
+            </div>
+            <div className={'review 2'}>
+                <h3>Review 2</h3>
+                {/*<p>{userdata.comments}</p>*/}
+            </div>
+
+        </CommentsContainer>
+        <RightContainer>
+            {/*<h3>About {userdata.name}</h3>*/}
+            <div className={'location2'}>
+                <h3>Location</h3>
+                {/*<p>{userdata.location}</p>*/}
+            </div>
+            <div className={'review 1'}>
+                <h3>Luna member since</h3>
+                {/*<p>{userdata.created}</p>*/}
+            </div>
+            <div className={'things I love'}>
+                <h3>Things I love</h3>
+                {/*<p>{userdata.likes}</p>*/}
+            </div>
+            <div className={'description'}>
+                <h3>Description</h3>
+                {/*<p>{userdata.description}</p>*/}
+            </div>
+
+        </RightContainer>
+
+
+
+
+
+
+        <Footer/>
+
+    </>
+        )
+        {/*{error && <Header error />}*/}
+    </>
+  )
 }
+
 
 export default Profile;
