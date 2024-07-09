f21409f4a383e4190c68cf474a627f044db0fcd1
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-09 14:13:23 +0200

    backend registration - linting

--- backend/registration/views.py
+++ backend/registration/views.py
@@ -1,7 +1,6 @@
 from random import randint
 
 from django.core.mail import send_mail
-from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,2 +1,5 @@
+from rest_framework.generics import ListCreateAPIView
 
 
+class AllUsersView(ListCreateAPIView):
+    pass
