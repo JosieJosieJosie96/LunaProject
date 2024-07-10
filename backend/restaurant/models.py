3a72b16b32c83fee0e84f860d86a7676b24cd86c
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-10 17:56:42 +0200

    backend restaurant: - added picture upload to restaurant and choices for the category of the restaurant

--- backend/restaurant/models.py
+++ backend/restaurant/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 from project import settings
 
+def get_image_upload_path(instance, filename):
+    return f'{instance.id}/restaurant_pictures/{filename}'
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=20,
+                              choices=(('Asian', 'Asian'), ('Italian', 'Italian'), ('Burger', 'Burger'), ('Arab', 'Arab'), ('Mexican', 'Mexican')),
+                              default='none')
     country = models.CharField(max_length=100)
     street = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
@@ -15,5 +19,5 @@ class Restaurant(models.Model):
     email = models.CharField(max_length=100, null=True)
     opening_hours = models.CharField(max_length=100, null=True)
     price_level = models.CharField(null=True)
-    image = models.ImageField(upload_to='restaurant_images', null=True)
+    image = models.ImageField(upload_to=get_image_upload_path, null=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
