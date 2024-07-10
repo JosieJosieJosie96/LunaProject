6aed8fb7544672591f9a2961c95d4e01b77d0a70
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-10 15:58:15 +0200

    backend restaurant: - view for restaurant category - registration/validation repaired - changes on views with restaurants

--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -3,13 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from restaurant.models import Restaurant
-from restaurant.serializers import RestaurantCreateSerializer
+from restaurant.serializers import RestaurantCreateSerializer, RestaurantGetSerializer
 
 
 class RestaurantGetView(GenericAPIView):
     queryset = Restaurant.objects.all()
-    serializer_class = RestaurantCreateSerializer
-    permission_classes = (IsAuthenticated,)
+    serializer_class = RestaurantGetSerializer
+    permission_classes = []
 
     def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()
@@ -28,3 +28,23 @@ class RestaurantCreateView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
         return Response(serializer.data)
+
+class RestaurantCategoryView(GenericAPIView):
+    serializer_class = RestaurantGetSerializer
+    permission_classes = []
+
+    def get_queryset(self):
+        queryset = Restaurant.objects.all()
+        filter_option = self.kwargs.get('category')
+        if filter_option is not None:
+            return queryset.filter(category=filter_option)
+        return queryset
+
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+class RestaurantUserView(GenericAPIView):
+    pass
