09af1b2e86c5b5c53f0775bdd50874b37728dd35
Margarita Ferreira <margarita.p.ferreira@gmail.com>
2024-07-11 20:35:53 +0000

    Update file RestaurantPage.jsx

--- frontend/src/pages/RestaurantPage.jsx
+++ frontend/src/pages/RestaurantPage.jsx
@@ -1,47 +1,46 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import WriteReview from '../components/WriteReview';
 import {
-    ContainerAll,
-    HeroContainer,
-    HeroImage,
-    Input,
+  ContainerAll,
+  HeroContainer,
+  HeroImage,
+  Input,
 } from '../components/HeroHeader';
-import {Button} from '../../ui/Button';
+import { Button } from '../../ui/Button';
 import RestaurantPageReviewCard from '../components/RestaurantPageReviewCard';
 import styled from 'styled-components';
-import {useParams} from 'react-router-dom';
-import {getRestaurantByID} from '../api/api';
+import { useParams } from 'react-router-dom';
+import { getRestaurantByID } from '../api/api';
 
 const HeroTextContainer = styled.div`
-    position: absolute;
-    left: 20%;
-    top: 50%;
-    transform: translate(-50%, -50%);
+  position: absolute;
+  left: 20%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  h2 {
+    color: white;
+  }
 
-    h2 {
-        color: white;
-    }
-
-    p {
-        color: white;
-    }
+  p {
+    color: white;
+  }
 `;
 
 function RestaurantPage() {
-    const params = useParams();
-    const restaurantId = params.restaurantId;
-    const [restaurantData, setRestaurantData] = useState([]);
-    const [isClicked, setIsClicked] = useState(false);
-    const [isReviewButtonClicked, setIsReviewButtonClicked] = useState(false);
+  const params = useParams();
+  const restaurantId = params.restaurantId;
+  const [restaurantData, setRestaurantData] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
+  const [isReviewButtonClicked, setIsReviewButtonClicked] = useState(false);
 
-    useEffect(() => {
-        getRestaurantByID(setRestaurantData, restaurantId);
-    }, []);
+  useEffect(() => {
+    getRestaurantByID(setRestaurantData, restaurantId);
+  }, []);
 
-    return (
-        <>
-            <HeroContainer>
-                <HeroImage src={restaurantData?.image}/>
+  return (
+    <>
+      <HeroContainer>
+        <HeroImage src={restaurantData?.image} />
 
         <ContainerAll>
           <div
@@ -114,10 +113,70 @@ function RestaurantPage() {
                         <Button style={{ width: '20%' }}>FILTER</Button>
                       </div>
                     </div>
+                  </div>
+                  <RestaurantPageReviewCard
+                    setIsClicked={setIsClicked}
+                    isClicked={isClicked}
+                  />
                 </div>
-            )}
-        </>
-    );
+              </div>
+              <div
+                style={{
+                  width: '100%',
+                  backgroundColor: 'white',
+                  padding: '24px',
+                }}
+              >
+                <div
+                  style={{
+                    display: 'flex',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.105)',
+                    paddingBottom: '12px',
+                    alignItems: 'center',
+                  }}
+                >
+                  <img
+                    style={{ paddingRight: '12px' }}
+                    src="src/assets/svg/clock.svg"
+                  />
+                  <p>{restaurantData?.opening_hours}</p>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    paddingBottom: '14px',
+                    alignItems: 'center',
+                  }}
+                >
+                  <img
+                    style={{ paddingRight: '12px' }}
+                    src="src/assets/svg/money.svg"
+                  />
+                  <p>{restaurantData?.price_level}</p>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    gap: '7px',
+                    paddingTop: '20px',
+                  }}
+                >
+                  <Button
+                    onClick={() => setIsReviewButtonClicked((prev) => !prev)}
+                  >
+                    WRITE A REVIEW
+                  </Button>
+                  <Button>EDIT DATA</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default RestaurantPage;
+
