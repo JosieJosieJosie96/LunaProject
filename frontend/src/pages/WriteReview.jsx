bb10f3dc7e4a80000f18d0df8971895ed2a0e9ac
Josie Fessey <07jreckn@gmail.com>
2024-07-11 14:56:00 +0300

    commit

--- frontend/src/pages/WriteReview.jsx
+++ frontend/src/pages/WriteReview.jsx
@@ -1,38 +1,39 @@
 import {Button} from "../../ui/Button.jsx";
-import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
-import {LargeInput} from "../components/Profile.styled.js";
+import {FormContainer, LeftText, TextArea,} from "../components/LargeInput.js";
 
-function WriteReview() {
-  const { register, handleSubmit } = useForm();
+// eslint-disable-next-line react/prop-types
+const WriteReview = ({ onSubmit }) => {
+  const [reviewContent, setReviewContent] = useState('');
 
-  const onSubmit = (data) => {
-    console.log(data);
-    // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(Review.text_content);
+    setReviewContent('');
+  };
+
+  const handleChange = (e) => {
+    setReviewContent(e.target.value);
   };
 
   return (
-    <>
-      <p>Select your rating</p>
-      {/* Add your star rating component here */}
+    <FormContainer>
+      <form onSubmit={handleSubmit}>
+        <TextArea
+          placeholder="Your review helps others learn about great local businesses.
+Please don't review this business if you received a freebie for writing this review, or if you're connected in any way to the owner or employees."
+          value={reviewContent}
+          onChange={handleChange}
+          required
+        />
+        <Button type="submit">Submit Review</Button>
+      </form>
+      <LeftText>This field is required</LeftText>
+    </FormContainer>
 
-      <div style={{ marginTop: '40px' }}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <LargeInput
-            htmlFor="review"
-            type="text"
-            register={register}
-            name="review"
-            placeholder="Your review helps others learn about great local businesses.
-
-            Please don't review this business if you received a freebie for writing this review, or if you're connected in any way to the owner or employees."
-          />
-          <Button>Submit</Button>
-        </form>
-      </div>
-    </>
   );
-}
+};
 
 export default WriteReview;
 
