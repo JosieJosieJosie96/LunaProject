359b4498c8c0e01f2cb1b977de2b0939c75f6513
margarita2104 <margarita.p.ferreira@gmail.com>
2024-07-11 14:00:51 +0200

    added fonts

--- frontend/ui/Button.jsx
+++ frontend/ui/Button.jsx
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  border-radius: 8px;
-  background-color: #f59e0b;
-  cursor: pointer;
-  font-size: 16px;
-  color: white;
-  padding: 4px 24px;
-  border: none;
+    font-family: "Roboto", sans-serif;
+    border-radius: 8px;
+    background-color: #f59e0b;
+    cursor: pointer;
+    font-size: 16px;
+    color: white;
+    padding: 4px 24px;
+    border: none;
 
-  &:hover {
-    background-color: #d97706;
-    transform: scale(0.95);
-  }
+    &:hover {
+        background-color: #d97706;
+        transform: scale(0.95);
+    }
 `;
