ea664813d15a9cbdae82be3c02e8ee7042c1fd07
Emir Murati <emir_810@icloud.com>
2024-07-12 09:17:01 +0200

    fixed some bugs in the review component

--- frontend/src/components/RestaurantCard.jsx
+++ frontend/src/components/RestaurantCard.jsx
@@ -1,93 +1,95 @@
 /* eslint-disable react/prop-types */
 
-import {Fragment, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import styled from 'styled-components';
 
 export const Card = styled.div`
-    border: 1px solid #EBEBEB;
-    border-top: 7px solid #f59e0b;
-    min-width: 270px;
-    max-width: 270px;
-    background-color: white;
-    border-radius: 5px;
+  border: 1px solid #ebebeb;
+  border-top: 7px solid #f59e0b;
+  min-width: 270px;
+  max-width: 270px;
+  background-color: white;
+  border-radius: 5px;
 `;
 
 export const Container = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
 const Image = styled.img`
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-    object-position: center;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  object-position: center;
 `;
 const ImageContainer = styled.div`
-    width: 100%;
-    height: 260px;
+  width: 100%;
+  height: 260px;
 `;
 
-function RestaurantCard({restaurants}) {
-    const navigate = useNavigate();
-    const [rating, setRating] = useState(0)
+function RestaurantCard({ restaurants }) {
+  const navigate = useNavigate();
 
-    function handleClick(id) {
-        navigate(`/restaurant/${id}`, {
-            replace: true,
-        });
-    }
+  function handleClick(id) {
+    navigate(`/restaurant/${id}`, {
+      replace: true,
+    });
+  }
 
-    return (
-        <>
-            {restaurants?.map((restaurant, index) => (
-                <Fragment key={index}>
-                    {!restaurant.image ? null : (
+  return (
+    <>
+      {restaurants?.map((restaurant, index) => (
+        <Fragment key={index}>
+          {!restaurant.image ? null : (
+            <div
+              onClick={() => handleClick(restaurant.id)}
+              style={{ cursor: 'pointer' }}
+            >
+              <Container>
+                <Card>
+                  <div
+                    style={{
+                      marginTop: '4px',
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      padding: '10px 24px',
+                    }}
+                  >
+                    <div>
+                      <h3>{restaurant.name}</h3>
+
+                      <p>{restaurant.street}</p>
+                      <div
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                          alignItems: 'center',
+                        }}
+                      >
                         <div
-                            onClick={() => handleClick(restaurant.id)}
-                            style={{cursor: 'pointer'}}
+                          style={{
+                            fontSize: '20px',
+                            color: '#4C4C4C',
+                          }}
                         >
-                            <Container>
-                                <Card>
-                                    <div
-                                        style={{
-                                            marginTop: '4px',
-                                            display: 'flex',
-                                            justifyContent: 'space-between',
-                                            padding: '10px 24px',
-                                        }}
-                                    >
-                                        <div>
-                                            <h3>{restaurant.name}</h3>
-
-                                            <p>{restaurant.street}</p>
-                                            <div style={{
-                                                display: 'flex',
-                                                justifyContent: 'space-between',
-                                                alignItems: 'center'
-                                            }}>
-                                                <RatingReview rating={rating} setRating={setRating
-                                                }/>
-                                                <div style={{
-                                                    fontSize: '20px',
-                                                    color: '#4C4C4C',
-                                                }}>21
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <ImageContainer>
-                                        <Image src={restaurant.image} alt="restaurant image"/>
-                                    </ImageContainer>
-                                </Card>
-                            </Container>
+                          21
                         </div>
-                    )}
-                </Fragment>
-            ))}
-        </>
-    );
+                      </div>
+                    </div>
+                  </div>
+                  <ImageContainer>
+                    <Image src={restaurant.image} alt="restaurant image" />
+                  </ImageContainer>
+                </Card>
+              </Container>
+            </div>
+          )}
+        </Fragment>
+      ))}
+    </>
+  );
 }
 
 export default RestaurantCard;
--- frontend/src/pages/WriteReview.jsx
+++ frontend/src/pages/WriteReview.jsx
@@ -1,58 +1,58 @@
-import {Button} from "../../ui/Button.jsx";
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import {FormContainer, TextArea,} from "../components/LargeInput.js";
-import axios from "axios";
+// import {Button} from "../../ui/Button.jsx";
+// import { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import {FormContainer, TextArea,} from "../components/LargeInput.js";
+// import axios from "axios";
 
-// eslint-disable-next-line react/prop-types
-const WriteReview =
-({ restaurantId }) => {
-  const [reviewContent, setReviewContent] = useState('');
-  const [error, setError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState('');
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        `http://localhost:8000/api/reviews/new/${restaurantId}/`,
-        {
-          content: reviewContent,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        }
-      );
-      setSuccessMessage('Review submitted successfully!', response.data);
-      setReviewContent(''); // Clear textarea after submission
-    } catch (error) {
-      setError('Error submitting review');
-    }
-  };
+// // eslint-disable-next-line react/prop-types
+// const WriteReview =
+// ({ restaurantId }) => {
+//   const [reviewContent, setReviewContent] = useState('');
+//   const [error, setError] = useState(null);
+//   const [successMessage, setSuccessMessage] = useState('');
+// const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await axios.post(
+//         `http://localhost:8000/api/reviews/new/${restaurantId}/`,
+//         {
+//           content: reviewContent,
+//         },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem('token')}`,
+//           },
+//         }
+//       );
+//       setSuccessMessage('Review submitted successfully!', response.data);
+//       setReviewContent(''); // Clear textarea after submission
+//     } catch (error) {
+//       setError('Error submitting review');
+//     }
+//   };
 
-  const handleChange = (e) => {
-    setReviewContent(e.target.value);
-  };
+//   const handleChange = (e) => {
+//     setReviewContent(e.target.value);
+//   };
 
-  return (
-    <FormContainer>
-      <form onSubmit={handleSubmit}>
-        <TextArea
-          placeholder="Your review helps others learn about great local businesses.
-Please don't review this business if you received a freebie for writing this review, or if you're connected in any way to the owner or employees."
-          value={reviewContent}
-          onChange={handleChange}
-          required
-        />
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
-        <Button type="submit">Submit Review</Button>
-      </form>
-    </FormContainer>
-  );
-};
-WriteReview.propTypes = {
-  restaurantId: PropTypes.number.isRequired,
-};
-export default WriteReview;
+//   return (
+//     <FormContainer>
+//       <form onSubmit={handleSubmit}>
+//         <TextArea
+//           placeholder="Your review helps others learn about great local businesses.
+// Please don't review this business if you received a freebie for writing this review, or if you're connected in any way to the owner or employees."
+//           value={reviewContent}
+//           onChange={handleChange}
+//           required
+//         />
+//         {error && <p style={{ color: 'red' }}>{error}</p>}
+//         {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+//         <Button type="submit">Submit Review</Button>
+//       </form>
+//     </FormContainer>
+//   );
+// };
+// WriteReview.propTypes = {
+//   restaurantId: PropTypes.number.isRequired,
+// };
+// export default WriteReview;
